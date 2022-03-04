e3f290709ff991c7fbabe04a4ae7c1ce11575661
Karol Wilczynski <wilczynski87@gmail.com>
2022-03-04 15:19:42 +0100

    First commit

---
+++ src/main/java/com/qaproject/demo/SpringProject11Application.java
@@ -0,0 +1,13 @@
+package com.qaproject.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProject11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProject11Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/qaproject/demo/SpringProject11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qaproject.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProject11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
